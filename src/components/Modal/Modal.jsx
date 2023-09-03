37e6571c9c49a562ce0cde5a4838eae6b1bc3d9b
MihailLeontev <mihailleontev177@gmail.com>
2023-09-03 23:25:37 +0300

    RHW
    initial commit + fix

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import css from './Modal.module.css';
+import PropTypes from 'prop-types';
+
+const Modal = ({ onClose, largeImageURL }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown); 
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown); 
+    };
+  }, [onClose]);
+
+  return (
+    <div className={css.overlay} onClick={onClose}>
+      <div className={css.modal}>
+        <img className={css.modal__image} src={largeImageURL} alt="" />
+      </div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  onClose: PropTypes.func,
+  largeImageURL: PropTypes.string,
+};
+
+export default Modal;
---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const fetchImages = async (query, page) => {
+  const response = await axios.get(`https://pixabay.com/api/`, {
+    method: 'get',
+    params: {
+      key: '34555959-7e3993bd8ec648ace59152f2a',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 12,
+      page: page,
+    },
+  });
+
+  return response.data;
+};
+
+export default fetchImages;
